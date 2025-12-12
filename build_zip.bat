8fc11563dce7a1fe291bffb8c0bcc9eccb5e813f
Boris Barbulovski <bbarbulovski@gmail.com>
2025-12-13 00:04:19 +0100

    Add cfrds.lib zip archive.

--- build_zip.bat
+++ build_zip.bat
@@ -7,9 +7,10 @@ for /f "delims=" %%i in ('git describe --tags --dirty') do set git_describe=%%i
 mkdir cfrds\include
 copy bin\cfrds.exe cfrds
 copy bin\cfrds.dll cfrds
+copy bin\cfrds.lib cfrds
 copy bin\libxml2.dll cfrds
 copy include\cfrds.h cfrds\include
 copy include\wddx.h cfrds\include
 
-tar -a -c -f "cfrds-%git_describe%.zip" "cfrds\cfrds.exe" "cfrds\cfrds.dll" "cfrds\libxml2.dll" "cfrds\include\cfrds.h" "cfrds\include\wddx.h"
+tar -a -c -f "cfrds-%git_describe%.zip" "cfrds\cfrds.exe" "cfrds\cfrds.dll" "cfrds\cfrds.lib" "cfrds\libxml2.dll" "cfrds\include\cfrds.h" "cfrds\include\wddx.h"
 rmdir /s /q cfrds

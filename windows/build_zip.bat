a7980f4af2d4d9f1e499ab38ff3dbcd393c286e1
Boris Barbulovski <bbarbulovski@gmail.com>
2025-12-24 05:50:55 +0100

    Fix build_zip.bat

--- windows/build_zip.bat
+++ windows/build_zip.bat
@@ -5,12 +5,12 @@ cls
 for /f "delims=" %%i in ('git describe --tags --dirty') do set git_describe=%%i
 
 mkdir cfrds\include
-copy bin\cfrds.exe cfrds
-copy bin\cfrds.dll cfrds
-copy bin\cfrds.lib cfrds
-copy bin\libxml2.dll cfrds
-copy include\cfrds.h cfrds\include
-copy include\wddx.h cfrds\include
+copy ..\bin\cfrds.exe cfrds
+copy ..\bin\cfrds.dll cfrds
+copy ..\bin\cfrds.lib cfrds
+copy ..\bin\libxml2.dll cfrds
+copy ..\include\cfrds.h cfrds\include
+copy ..\include\wddx.h cfrds\include
 
 tar -a -c -f "cfrds-%git_describe%.zip" "cfrds\cfrds.exe" "cfrds\cfrds.dll" "cfrds\cfrds.lib" "cfrds\libxml2.dll" "cfrds\include\cfrds.h" "cfrds\include\wddx.h"
 rmdir /s /q cfrds

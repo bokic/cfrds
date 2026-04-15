fd1a923302042738f79e3163213640db128274d8
Boris Barbulovski <bbarbulovski@gmail.com>
2026-04-15 03:07:41 +0200

    Build tracing with RelWithDebuInfo

--- build_with_tracing.sh
+++ build_with_tracing.sh
@@ -4,5 +4,5 @@ set -e
 
 rm -rf build
 
-cmake -B build -DENABLE_PERFETTO=ON -DCMAKE_BUILD_TYPE=Debug
-cmake --build build  --config Debug
+cmake -B build -DENABLE_PERFETTO=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build build --config RelWithDebInfo

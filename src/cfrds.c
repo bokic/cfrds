f45e0daa79b29c5d89b7709f9ad0575e1f4eae89
Boris Barbulovski <bbarbulovski@gmail.com>
2026-08-04 04:21:53 +0200

    cfrds: deduplicate DB command execution boilerplate with helper

--- src/cfrds.c
+++ src/cfrds.c
@@ -446,279 +446,113 @@ cfrds_status cfrds_command_file_get_root_dir(cfrds_server *server, cfrds_str *ou
     return ret;
 }
 
-cfrds_status cfrds_command_sql_dsninfo(cfrds_server *server, cfrds_sql_dsninfo **dsninfo)
-{
-    cfrds_status ret;
+typedef void *(*cfrds_sql_parser_fn)(cfrds_buffer *buffer);
 
+static cfrds_status cfrds_execute_sql_cmd(cfrds_server *server, const char *params[], cfrds_sql_parser_fn parser, void **out_result)
+{
     cfrds_buffer_defer(response);
 
-    if ((server == NULL)||(dsninfo == NULL))
-    {
-        return CFRDS_STATUS_PARAM_IS_NULL;
-    }
-
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ "", "DSNINFO", NULL});
+    cfrds_status ret = cfrds_send_command(server, &response, "DBFUNCS", params);
     if (ret == CFRDS_STATUS_OK)
     {
-        *dsninfo = cfrds_buffer_to_sql_dsninfo(response);
-        if (*dsninfo == NULL)
+        void *res = parser(response);
+        if (res == NULL)
         {
             server->error_code = -1;
             return CFRDS_STATUS_RESPONSE_ERROR;
         }
+        *out_result = res;
     }
 
     return ret;
+}
+
+cfrds_status cfrds_command_sql_dsninfo(cfrds_server *server, cfrds_sql_dsninfo **dsninfo)
+{
+    if ((server == NULL) || (dsninfo == NULL))
+        return CFRDS_STATUS_PARAM_IS_NULL;
+
+    return cfrds_execute_sql_cmd(server, (const char *[]){ "", "DSNINFO", NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_dsninfo, (void **)dsninfo);
 }
 
 cfrds_status cfrds_command_sql_tableinfo(cfrds_server *server, const char *connection_name, cfrds_sql_tableinfo **tableinfo)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(connection_name == NULL)||(tableinfo == NULL))
-    {
+    if ((server == NULL) || (connection_name == NULL) || (tableinfo == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ connection_name, "TABLEINFO", NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *tableinfo = cfrds_buffer_to_sql_tableinfo(response);
-        if (*tableinfo == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ connection_name, "TABLEINFO", NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_tableinfo, (void **)tableinfo);
 }
 
 cfrds_status cfrds_command_sql_columninfo(cfrds_server *server, const char *connection_name, const char *table_name, cfrds_sql_columninfo **columninfo)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(columninfo == NULL))
-    {
+    if ((server == NULL) || (columninfo == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ connection_name, "COLUMNINFO", table_name, NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *columninfo = cfrds_buffer_to_sql_columninfo(response);
-        if (*columninfo == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ connection_name, "COLUMNINFO", table_name, NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_columninfo, (void **)columninfo);
 }
 
 cfrds_status cfrds_command_sql_primarykeys(cfrds_server *server, const char *connection_name, const char *table_name, cfrds_sql_primarykeys **primarykeys)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(table_name == NULL))
-    {
+    if ((server == NULL) || (table_name == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ connection_name, "PRIMARYKEYS", table_name, NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *primarykeys = cfrds_buffer_to_sql_primarykeys(response);
-        if (*primarykeys == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ connection_name, "PRIMARYKEYS", table_name, NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_primarykeys, (void **)primarykeys);
 }
 
 cfrds_status cfrds_command_sql_foreignkeys(cfrds_server *server, const char *connection_name, const char *table_name, cfrds_sql_foreignkeys **foreignkeys)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(table_name == NULL))
-    {
+    if ((server == NULL) || (table_name == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ connection_name, "FOREIGNKEYS", table_name, NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *foreignkeys = cfrds_buffer_to_sql_foreignkeys(response);
-        if (*foreignkeys == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ connection_name, "FOREIGNKEYS", table_name, NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_foreignkeys, (void **)foreignkeys);
 }
 
 cfrds_status cfrds_command_sql_importedkeys(cfrds_server *server, const char *connection_name, const char *table_name, cfrds_sql_importedkeys **importedkeys)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(table_name == NULL))
-    {
+    if ((server == NULL) || (table_name == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ connection_name, "IMPORTEDKEYS", table_name, NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *importedkeys = cfrds_buffer_to_sql_importedkeys(response);
-        if (*importedkeys == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ connection_name, "IMPORTEDKEYS", table_name, NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_importedkeys, (void **)importedkeys);
 }
 
 cfrds_status cfrds_command_sql_exportedkeys(cfrds_server *server, const char *connection_name, const char *table_name, cfrds_sql_exportedkeys **exportedkeys)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(table_name == NULL))
-    {
+    if ((server == NULL) || (table_name == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ connection_name, "EXPORTEDKEYS", table_name, NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *exportedkeys = cfrds_buffer_to_sql_exportedkeys(response);
-        if (*exportedkeys == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ connection_name, "EXPORTEDKEYS", table_name, NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_exportedkeys, (void **)exportedkeys);
 }
 
 cfrds_status cfrds_command_sql_sqlstmnt(cfrds_server *server, const char *connection_name, const char *sql, cfrds_sql_resultset **resultset)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(resultset == NULL))
-    {
+    if ((server == NULL) || (resultset == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ connection_name, "SQLSTMNT", sql, NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *resultset = cfrds_buffer_to_sql_sqlstmnt(response);
-        if (*resultset == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ connection_name, "SQLSTMNT", sql, NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_sqlstmnt, (void **)resultset);
 }
 
 cfrds_status cfrds_command_sql_sqlmetadata(cfrds_server *server, const char *connection_name, const char *sql, cfrds_sql_metadata **metadata)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(metadata == NULL))
-    {
+    if ((server == NULL) || (metadata == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ connection_name, "SQLMETADATA", sql, NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *metadata = cfrds_buffer_to_sql_metadata(response);
-        if (*metadata == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ connection_name, "SQLMETADATA", sql, NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_metadata, (void **)metadata);
 }
 
 cfrds_status cfrds_command_sql_getsupportedcommands(cfrds_server *server, cfrds_sql_supportedcommands **supportedcommands)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(supportedcommands == NULL))
-    {
+    if ((server == NULL) || (supportedcommands == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ "", "SUPPORTEDCOMMANDS", NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *supportedcommands = cfrds_buffer_to_sql_supportedcommands(response);
-        if (*supportedcommands == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ "", "SUPPORTEDCOMMANDS", NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_supportedcommands, (void **)supportedcommands);
 }
 
 cfrds_status cfrds_command_sql_dbdescription(cfrds_server *server, const char *connection_name, cfrds_str *description)
 {
-    cfrds_status ret;
-
-    cfrds_buffer_defer(response);
-
-    if ((server == NULL)||(description == NULL))
-    {
+    if ((server == NULL) || (description == NULL))
         return CFRDS_STATUS_PARAM_IS_NULL;
-    }
 
-    ret = cfrds_send_command(server, &response, "DBFUNCS", (const char *[]){ connection_name, "DBDESCRIPTION", NULL});
-    if (ret == CFRDS_STATUS_OK)
-    {
-        *description = cfrds_buffer_to_sql_dbdescription(response);
-        if (*description == NULL)
-        {
-            server->error_code = -1;
-            return CFRDS_STATUS_RESPONSE_ERROR;
-        }
-    }
-
-    return ret;
+    return cfrds_execute_sql_cmd(server, (const char *[]){ connection_name, "DBDESCRIPTION", NULL }, (cfrds_sql_parser_fn)cfrds_buffer_to_sql_dbdescription, (void **)description);
 }
 
 void cfrds_file_content_free(cfrds_file_content *value)
